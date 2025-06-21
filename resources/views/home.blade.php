cad1025062cd198f065b7e5a70bad4d3ac4b5464
lucasclemerson <clemerson.lucas.oliveira@gmail.com>
2025-06-21 18:33:26 -0300

    Finalização da aba tools

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,26 +20,57 @@
         @include('home.footer')
   
         <script>
-            function scrollToId(id) {
-              const element = document.getElementById(id);
-              if (element) {
-                element.scrollIntoView({ behavior: 'smooth' });
-              }
+            const corosselContainer = document.getElementsByClassName("carossel-Container");
+            for (let index = 0; index < corosselContainer.length; index++) {
+                let corosselItems = corosselContainer[index];
+                
+                let btnBack = document.getElementById('btnBack')
+                let btnNext = document.getElementById('btnNext')
+                let itens = corosselItems.getElementsByClassName('item')
+
+                let posicaoAnterior = 0
+                let mover = posicaoAnterior + 100
+                
+                
+                btnNext.addEventListener('click', ()=>{
+                    mover = posicaoAnterior + 100
+                    if (mover < 200){
+                        for(let c = 0; c < itens.length; c++ ){
+                        itens[c].style.right = mover + '%'
+                        posicaoAnterior = mover
+                        }
+                    }else{
+                        for(let c = 0; c < itens.length; c++ ){
+                            itens[c].style.right = '0%'
+                            posicaoAnterior = 0
+                            mover = 0
+                        }
+                    }
+                })
+
+                
+                btnBack.addEventListener('click', ()=>{
+                    mover = posicaoAnterior - 100
+                    if (mover > 0){
+                        for(let c = 0; c < itens.length; c++ ){
+                        itens[c].style.right = mover + '%'
+                        posicaoAnterior = mover
+                        }
+                    }else{
+                        for(let c = 0; c < itens.length; c++ ){
+                            itens[c].style.right = '0%'
+                            posicaoAnterior = 0
+                            mover = 0
+                        }
+                    }
+                })
             }
-
-            const carousel = document.getElementById('carousel');
-            const next = document.getElementById('nextBtn');
-            let haveOne = false;
-
             function scrollNext() {
                 carousel.scrollBy({
                     left: carousel.offsetWidth,
                     behavior: 'smooth'
                 });
             }
-
-            next.addEventListener('click', scrollNext);
-            setInterval(scrollNext, 5000);
         </script>
     </body>
 </html>
